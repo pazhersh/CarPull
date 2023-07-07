b386ed8620a7fa7d4072124312156a01ba184f66
Venfi <venfioranai@gmail.com>
2023-07-07 23:59:18 +0300

    Removed redundant logic

--- scripts/car.gd
+++ scripts/car.gd
@@ -5,11 +5,15 @@ const PULL_POWER = 1000.0
 const MAX_ROW = 2
 const MAX_COLUMN = 1
 
-func _physics_process(delta):
-	var person1 = Person.new(1, 1, "Row 1 Person 1")
-	var person2 = Person.new(2, 1, "Row 2 Person 1")
-	var people = [person1, person2]
-	
+var people: Array = []
+
+func _ready():
+	people.append_array([
+		Person.new(1, 1, "Row 1 Person 1"),
+		Person.new(2, 1, "Row 2 Person 1")
+	])
+
+func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var poeple_movement = people.reduce(func(acc, current): return acc + current.process(), Vector2())
