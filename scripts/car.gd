949b07ffc50a1853eafaf13e093f7bda95155fc0
Venfi <venfioranai@gmail.com>
2023-07-08 00:52:04 +0300

    Fixed variable name typo

--- scripts/car.gd
+++ scripts/car.gd
@@ -7,10 +7,9 @@ var motion = Vector2()
 func calculate_movement() -> Vector2:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	# var poeple_movement = people.reduce(func(acc, current): return acc + current.process(), Vector2())
-	var poeple_movement = get_tree().get_nodes_in_group("people").reduce(func(acc, current): return acc + current.process(), Vector2())
-	rotation += poeple_movement.y / 25
-	motion += poeple_movement.rotated(rotation)
+	var people_movement = get_tree().get_nodes_in_group("people").reduce(func(acc, current): return acc + current.process(), Vector2())
+	rotation += people_movement.y / 25
+	motion += people_movement.rotated(rotation)
 	
 	var deceleration = -motion.normalized() * DECELERATION
 	if VectorUtils.are_vectors_in_opposite_directions(motion + deceleration, motion):
