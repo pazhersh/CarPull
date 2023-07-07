6cab88dfcb98fc0ca7dcb99d51e7e76a927e2017
Venfi <venfioranai@gmail.com>
2023-07-08 00:51:33 +0300

    Removed unused class

--- scripts/car.gd
+++ scripts/car.gd
@@ -1,16 +1,8 @@
 extends CharacterBody2D
-var Person = load('res://scripts/person.gd')
 
 const DECELERATION = 0.5
 
-var people: Array = []
 var motion = Vector2()
-
-func _ready():
-	people.append_array([
-		Person.new(1, 2, "Row 1 Person 1"), # changed to col=2 for a more fun angle
-		Person.new(2, 2, "Row 2 Person 1")  # TODO: change the angle calculation in Person
-	])
 
 func calculate_movement() -> Vector2:
 	# Get the input direction and handle the movement/deceleration.
