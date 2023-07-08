8e95f56a5d74a48cab3853054e3197b1c35ab17f
pz <pazhersh@gmail.com>
2023-07-08 11:13:06 +0300

    fixed [esc] menu

--- scenes/Tutorial-Level.gd
+++ scenes/Tutorial-Level.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-@export var main_menu: PackedScene;
-
 func _process(delta):
 	if Input.is_action_just_pressed('quit'):
-		get_tree().change_scene_to_file(main_menu.resource_path)
+		get_tree().change_scene_to_file('res://scenes/main_menu.tscn')
