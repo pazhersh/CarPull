a435cddd9fbd8e9fbc55d6fd24d5b27d14ca5918
pz <pazhersh@gmail.com>
2023-07-08 12:37:48 +0300

    added multiplayer

--- scripts/person-sprite.gd
+++ scripts/person-sprite.gd
@@ -4,13 +4,15 @@ extends Sprite2D
 @export var stunned_sprite: CompressedTexture2D = load('res://assets/person lying 1.png')
 @export var invincible_sprite: CompressedTexture2D = load('res://assets/person green 1.png')
 
-@export var textures = {
+var textures = {
 	PersonState.OK: ok_sprite,
 	PersonState.STUNNED: stunned_sprite,
 	PersonState.INVINCIBLE: invincible_sprite
 }
 
 func _ready():
+	print(PersonState.OK)
+	print(textures[PersonState.OK])
 	texture = textures[PersonState.OK]
 
 # set_texture is already a real thing on Sprite
