187d1af6ad1dff19fb7db97d1d812cf99a4e7ed2
Venfi <venfioranai@gmail.com>
2023-07-08 14:23:55 +0300

    Camera has offset

---
+++ scripts/camera.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+var player: Node
+var camera_offset: Vector2 = Vector2(100, 0)
+
+func _ready():
+	player = get_tree().get_first_node_in_group('player')
+
+func _process(delta):
+	var pt = player.get_transform()
+	transform = Transform2D(pt.x + camera_offset, pt.y, camera_offset)
