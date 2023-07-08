c23ea1b8085b0e0d2a342c554a7f80358ab046ca
Venfi <venfioranai@gmail.com>
2023-07-08 10:39:18 +0300

    Split person state enum off into file

--- scripts/person-sprite.gd
+++ scripts/person-sprite.gd
@@ -1,14 +1,14 @@
 extends Sprite2D
 
-@export var textures = {
-	'ok': 'res://assets/person 1.png',
-	'stunned': 'res://assets/person lying 1.png',
-	'invincible': 'res://assets/person green 1.png'
+const textures = {
+	PersonState.OK: 'res://assets/person 1.png',
+	PersonState.STUNNED: 'res://assets/person lying 1.png',
+	PersonState.INVINCIBLE: 'res://assets/person green 1.png'
 }
 
 func _ready():
-	texture = load(textures['ok'])
+	texture = load(textures[PersonState.OK])
 
 # set_texture is already a real thing on Sprite
-func new_texture(new_texture):
-	texture = load(new_texture)
+func set_state(state: int):
+	texture = load(textures[state])
--- scripts/person.gd
+++ scripts/person.gd
@@ -7,15 +7,9 @@ const PULL_POWER = 5.0
 const STUN_TIME = 3
 const INVINCIBILITY_TIME = 5
 
-enum State {
-	OK,
-	STUNNED,
-	INVINCIBLE
-}
-
 var whipSprite: AnimatedSprite2D
 var personSprite: Sprite2D
-var state: State
+var state: int
 var state_time: float
 
 func get_input():
@@ -24,21 +18,14 @@ func get_input():
 func _ready():
 	whipSprite = get_children().filter(func(child): return child.name == 'Whip').front()
 	personSprite = get_children().filter(func(child): return child.name == 'Sprite').front()
-	var state = State.OK
+	var state = PersonState.OK
 	var state_time: float = 0
 	
 func play_animation():
-	match state:
-		State.STUNNED:
-			personSprite.new_texture(personSprite.textures['stunned'])
-		State.INVINCIBLE:
-			personSprite.new_texture(personSprite.textures['invincible'])
-		_:
-			personSprite.new_texture(personSprite.textures['ok'])
-			
+	personSprite.set_state(state)
 
 func process():
-	if state != State.STUNNED:
+	if state != PersonState.STUNNED:
 		if !whipSprite.is_playing():
 			whipSprite.play_once()
 			return self.position.normalized() * PULL_POWER
@@ -49,15 +36,15 @@ func _process(delta):
 	state_time -= delta
 	if state_time < 0:
 		match state:
-			State.STUNNED:
-				state = State.INVINCIBLE
+			PersonState.STUNNED:
+				state = PersonState.INVINCIBLE
 				state_time = INVINCIBILITY_TIME
 			_:
-				state = State.OK
+				state = PersonState.OK
 				state_time = 0
 
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("obstacles") && state == State.OK:
-		state = State.STUNNED
+	if body.is_in_group("obstacles") && state == PersonState.OK:
+		state = PersonState.STUNNED
 		state_time = STUN_TIME
