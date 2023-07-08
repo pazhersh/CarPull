8f728aaf435dd9779cf1e92f93f30e090e2234c0
pz <pazhersh@gmail.com>
2023-07-08 12:43:59 +0300

    2-players colors

--- scripts/person-sprite.gd
+++ scripts/person-sprite.gd
@@ -1,18 +1,16 @@
 extends Sprite2D
 
-@export var ok_sprite: CompressedTexture2D = load('res://assets/person 1.png')
-@export var stunned_sprite: CompressedTexture2D = load('res://assets/person lying 1.png')
-@export var invincible_sprite: CompressedTexture2D = load('res://assets/person green 1.png')
+@export var ok_sprite: CompressedTexture2D
+@export var stunned_sprite: CompressedTexture2D
+@export var invincible_sprite: CompressedTexture2D
 
-var textures = {
+@onready var textures = {
 	PersonState.OK: ok_sprite,
 	PersonState.STUNNED: stunned_sprite,
 	PersonState.INVINCIBLE: invincible_sprite
 }
 
 func _ready():
-	print(PersonState.OK)
-	print(textures[PersonState.OK])
 	texture = textures[PersonState.OK]
 
 # set_texture is already a real thing on Sprite
